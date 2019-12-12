c6e2e1938b78793d73da7efe09921ade9cf371d6
ZhaohengLi <zhaohengli.thu@gmail.com>
2019-12-12 16:15:22 +0800

    修改初始ip地址

--- Homework/boilerplate/main.cpp
+++ Homework/boilerplate/main.cpp
@@ -90,7 +90,7 @@ uint8_t output[2048];
 // 2: 10.0.2.1
 // 3: 10.0.3.1
 // 你可以按需进行修改，注意端序
-in_addr_t addrs[N_IFACE_ON_BOARD] = {0x0603a8c0, 0x0704a8c0, 0x0102000a,
+in_addr_t addrs[N_IFACE_ON_BOARD] = {0x0203a8c0, 0x0104a8c0, 0x0102000a,
                                      0x0103000a};
 
 // Multicast addr
