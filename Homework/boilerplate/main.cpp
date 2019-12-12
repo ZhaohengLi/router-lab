e4b625b876070527921919b2393832a80793b568
ZhaohengLi <zhaohengli.thu@gmail.com>
2019-12-12 16:33:27 +0800

    初始端口的设定和打印信息的设定

--- Homework/boilerplate/main.cpp
+++ Homework/boilerplate/main.cpp
@@ -85,13 +85,12 @@ void setSrcAddr(in_addr_t src_addr, uint8_t *buffer){
 
 uint8_t packet[2048];
 uint8_t output[2048];
-// 0: 10.0.0.1
-// 1: 10.0.1.1
-// 2: 10.0.2.1
-// 3: 10.0.3.1
+// 0: 192.168.3.2
+// 1: 192.168.4.1
+// 2: 10.0.0.1
+// 3: 10.0.1.1
 // 你可以按需进行修改，注意端序
-in_addr_t addrs[N_IFACE_ON_BOARD] = {0x0203a8c0, 0x0104a8c0, 0x0102000a,
-                                     0x0103000a};
+in_addr_t addrs[N_IFACE_ON_BOARD] = {0x0203a8c0, 0x0104a8c0, 0x0100000a, 0x0101000a};
 
 // Multicast addr
 // 224.0.0.9
